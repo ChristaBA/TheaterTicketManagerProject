65e229fafda01f1a1d780a6c63643911c66c47a9
Javinlangley <ljl0005@uah.edu>
2016-10-11 19:24:28 -0500

    Add files via upload

---
+++ UI/newEmptyPHP.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>   
+        body {background-color:blue}
+       
+        
+        img{
+            
+            width:100%
+            
+            
+        }    
+            
+        </style>    
+    </head>
+    <body>
+     <h1>Welcome to Hunstville theaters</h1>   
+     
+     
+     <h2>Now showing</h2>
+     
+     
+     
+<img src="thor.jpg" alt="HTML5 Icon" style="width:250px;height:250px;">
+  
+    
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Get Tickets!</button>
+  
+<img src="purge.jpg" alt="HTML5 Icon" style="width:250px;height:250px;">
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Get Tickets!</button>
+
+<img src="endersgame.jpg" alt="HTML5 Icon" style="width:250px;height:250px;">
+
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Get Tickets!</button>
+
+<h2> Employee login</h2>
+<button type="button" onclick="document.getElementById('demo').innerHTML = 'Hello JavaScript!'">Login</button>
+
+
+<?php
+
+ 
+ 
+ ?>
+        
+    </body>
+</html>    
+
