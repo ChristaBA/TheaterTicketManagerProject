3691bdc1a0e622fb3d9e810ddd953e9d8525d777
Javinlangley <ljl0005@uah.edu>
2016-11-01 16:38:50 -0500

    Add files via upload

--- UI/Employeelogin.php
+++ UI/Employeelogin.php
@@ -1,11 +1,12 @@
 <html>
 	<head>
-           
+            
 
 		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+                
 
                 <style>
-                  body{
+                   body{
     background-color:black;
     margin:0;
     padding:0;
@@ -92,32 +93,125 @@
         
         
     }
-                    
-                    
-                    
-                    
-                    
+input[type=text], select {
+    width: 70%;
+    margin-left:25%;
+    margin-right:25%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 70%;
+    margin-left:25%;
+    margin-right:25%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+.Login {
+    
+    width:50%;
+    height:20%;
+    margin-right:25%;
+    margin-left:25%;
+    border-radius: 5px;
+    background-color:white;
+    padding: 10px;
+}
+  .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+    width:20%;
+
+      }
+.button1 {
+    margin-left:25%;
+    background-color: white;
+    color: black;
+    border: 2px solid #4CAF50;
+}
+.button1:hover {
+    background-color: #4CAF50;
+    color: white;
+}                  
+     
+.button2 {
+    background-color: white;
+    color: black;
+    border: 2px solid #f44336;
+}
+.button2:hover {
+    background-color: #f44336;
+    color: white; 
+
+
+.label{
+ 
+ margin-left:15%
+ 
+ 
+ }
+                  
                 </style>   
                 
-                
-                
+              
                 
                 
 	</head>
 	<body>
 	<script>
-	//All Jquery is supposed to go inside this function
+//All Jquery is supposed to go inside this function
 	$(document).ready(function() {
 			//Adds an event listener to the Ticket Submit button that waits until it's clicked
 		
 			$("#btnLogIn").click(function() {
+			console.log("Log in button clicked");
+			//window.location = "http://stackoverflow.com";
+			
 				//"serializes" the data from each field in the form into an array of Name/Value pairs and assigns that to the data variable
 				//Here is the documentation on this function if you wanna look at it
 				//http://api.jquery.com/serializeArray/
+				
+				//++++++++++++++++++
+				//You'll want to copy the following lines and change the URLs as needed
 				var data = $("#loginForm").serializeArray();
-				var OutputString = ""+data[0].name+": "+data[0].value+"<br>"+data[1].name+": "+data[1].value;
+				
+				if(data[1].value == "super")
+				{
+					window.location = "Super.html";
+				}
+				else if(data[1].value == "admin")
+				{
+					window.location = "Administrator.html";
+				}
+				//++++++++++++++++++
+				
+				//var OutputString = ""+data[0].name+": "+data[0].value+"<br>"+data[1].name+": "+data[1].value;
 				//console.log(OutputString);
-				$("#loginDisplay").html(OutputString);
+				//$("#loginDisplay").html(OutputString);
 				//sends the ajax request to the server
 				//The arguments used in the function are, in order: 
 				//1) the URL of the PHP file that will handle the request (in this case "your_PHP_file_here.php"),
@@ -125,9 +219,10 @@
 				//3) the function to handle the response if the request is successful
 				//
 				//I can explain this a bit more in detail when we start working on the back end stuff. Or whenever you guys wanna know exactly how it works.
-				$.post("your_PHP_file_here.php", data, function(response) {
+				//$.post("your_PHP_file_here.php", data, function(response) {
 					//Your code to handle the response from the PHP file will go here
-				});
+				//});
+				
 			});
 	});
 		</script>
@@ -154,24 +249,19 @@
               
           </div>
                 
+                <h1>Employee Login</h1>  
                 
                 
-                
-                <div id="TopBanner" class="logininput">
-		<form id="loginForm" class="login">
-			<label>Username:</label><input type="text" name="username"><br>
-			<label>Password:</label><input type="password" name="password"><br>
-			<button  class = "LoginButton" type="button" id="btnLogIn" >Log In</button>
-		</form>
-                  
+                <div class ="Login" >
+                    <form id="loginForm">
+			<label class ="label">Username:</label><input type="text" name="userName"><br>
+			<label class ="label">Password:</label><input type="text" name="password"><br>
+			<button  class = "button button1" type="button" id="btnLogIn">Log In</button>
+                        <button  class = "button button2" type="button" id="Cancel">Cancel</button>
+                        
                     
-                    
-                    
-		<article id="loginDisplay" class="loginDisplay"></article>
+                    </form>
+		
         </div>
-         <input type="button"  value="Admin" onclick="location='Admin.php'" />       
-         <br>
-         <br>
-           <input type="button"  value="Super User" onclick="location='GroupSuper.php'" />    
 	</body>
-</html> 
+</html>
