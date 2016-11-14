289b684270c19b6a044643bd8f7ce85ce9de6d3b
vatsalpb <vatsalpb@gmail.com>
2016-11-14 16:16:26 -0600

    Add files via upload
    You can adds images to the database when you create shows.

---
+++ PHP/ShowInsert.php
@@ -0,0 +1,78 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "testdatabase";
+// Create connection
+$link =  mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+} 
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+//if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      //  echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.<br>";
+        $uploadOk = 0;
+    }
+//}
+    // Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+    $uploadOk = 0;
+}
+$showname = filter_input(INPUT_POST,"showname");
+$startDate = filter_input(INPUT_POST,"startDate");
+$endDate = filter_input(INPUT_POST,"endDate");
+$location = filter_input(INPUT_POST,"location");
+// Escape user inputs for security
+$show_name = mysqli_real_escape_string($link, $showname);
+$start_date = mysqli_real_escape_string($link, $startDate);
+$end_Date = mysqli_real_escape_string($link, $endDate);
+$locationDB = mysqli_real_escape_string($link, $location);
+$imagetmp=addslashes (file_get_contents($_FILES['fileToUpload']['tmp_name']));
+$mysqli_get_users = mysqli_query($link,"SELECT * FROM showname where showName= '$show_name' " );
+$get_rows = mysqli_affected_rows($link);
+if($get_rows >= 1)
+{
+    echo "Show exists: please pick a different Showname.<br>";
+    echo "You are being redirected.<br>";
+    header("refresh:3; url = index.php");
+}
+else
+{
+    if ($check !== false)
+    {
+        $sql = "INSERT INTO showName (showname, startdate, enddate, location, image)
+        VALUES ('$show_name', '$start_date','$end_Date', '$locationDB', '$imagetmp')";
+            if ($link->query($sql) === TRUE) 
+            {
+                echo "New record created successfully<br>";
+            }
+            else 
+            {
+                echo "Error: " . $sql . "<br>" . $link->error;
+            }
+    }
+    else
+    {
+        echo "Show counld not be added to the database please try again.<br>";
+        echo "You are being redirected.<br>";
+        header("refresh:3; url = index.php");
+    }
+}
+$link->close();
---
+++ PHP/index.php
@@ -0,0 +1,144 @@
+<?php
+          
+        /*  $servername = "localhost:3306";
+            $username = "root";
+            $password = "";
+            $dbname = "testdatabase";
+
+$link =  mysqli_connect($servername, $username, $password, $dbname);
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+} 
+
+$query = "SELECT * FROM showname";
+$result1 = mysqli_query($link, $query);*/
+
+?>
+
+<!DOCTYPE.html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type"
+              content="text/html;
+              charset=UTF-8">
+        <title>Test Project</title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+    </head>
+     <body>
+         <div id="main">
+       <body style="background:#E66C6C">
+           
+            <input type="button" value="Home Page" onclick="location='HomePage.php'" />
+                
+         <input type="button" value="Create Database" onclick="location='myDB.php'" />
+                
+
+         <br>
+         <input type="button" value="Export File" onclick="location='upload.php'" />
+         <br>
+         <br>
+          <form action="ShowInsert.php" method="post"  enctype="multipart/form-data">
+    <p>
+        <label for="showName">Show Name:</label>
+        <input type="text" name="showname" id="showName">
+    </p> 
+    <p>
+       <label for="statDate">Start Date:</label>
+       <input type="date" name="startDate" id="startDate">
+    </p>
+    <p>
+        <label for="endDate">End Date:</label>
+       <input type="date" name="endDate" id="endDate">
+    </p>
+    <p>
+        <label for="location">Location:</label>
+        <input type="text" name="location" id="location">
+    </p>
+     Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Submit">
+</form>
+           <br>
+         <br>
+           <br>
+         <br>  <br>
+         <br>
+         <p>Show Names</p>
+         <select>
+             <?php while($row1 = mysqli_fetch_array($result1)):;?>
+             <option><?php echo $row1[0];?></option>
+             <?php endwhile;?>
+         </select>
+
+         <form action="AccountInsert.php" method="post">
+    <p>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="firstname" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastname" id="lastName">
+    </p>
+    <p>
+        <label for="password">Password:</label>
+         <input type="password" name="pass" id="password">
+    </p>
+    <p>
+        <label for="login">Login:</label>
+        <input type="text" name="log" id="login">
+    </p>
+     <p>
+        <label for="accountType">Account Type:</label>
+        <input type="number" name="acc" id="accountType">
+    </p>
+      <p>
+        <label for="companyName">Company Name:</label>
+        <input type="text" name="companyName" id="companyName">
+    </p>
+    <input type="submit" value="Submit">
+</form>
+         <h2>Alice In Wonderland</h2>
+          <img src="AliceInWonderland-Behance-M1.jpg" alt="AliceInWonderland" style="width:600px;height:800px;">
+     
+          
+          <form action="SeasonTicketInsert.php" method="post">
+    <p>
+       <label for="firstName">First Name:</label>
+        <input type="text" name="firstname" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastname" id="lastName">
+    </p>
+    <p>
+        <label for="ticketnumbe">Ticket Number:</label>
+       <input type="text" name="ticketNumber" id="ticketNumber">
+    </p>
+    <p>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email">
+    </p>
+     <p>
+        <label for="phonenumber">Phone Number:</label>
+        <input type="number" name="phoneNumber" id="phoneNumber">
+    </p>
+     <p>
+        <label for="seat">Seat:</label>
+        <input type="text" name="seat" id="seat">
+    </p>
+    <p>
+        <label for="day">Day:</label>
+        <input type="text" name="day" id="day">
+    </p>
+    <p>
+        <label for="time">Time:</label>
+        <input type="text" name="time" id="time">
+    </p>
+    <p>
+        <label for="address">Address:</label>
+        <input type="text" name="address" id="address">
+    </p>
+    <input type="submit" value="Submit">
+</form>   
+    </body>
+</html>
