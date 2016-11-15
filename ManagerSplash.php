30c2e787b6a8b1256e958e4f51bd06f0762cf487
ChristaBA <bac0012@uah.edu>
2016-11-15 16:53:00 -0600

    Add files via upload

--- ManagerSplash.php
+++ ManagerSplash.php
@@ -8,11 +8,23 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Manager Splash</title>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
     </head>
     <body>
+        <script>
+            $(document).ready(function() {
+                $.post("getSeasonTickets.php", function(response){
+                   $("#seasonTicketDisplay").html(response);
+                   //console.log(response);
+                });
+            });
+        </script>
         <?php
         // put your code here
         ?>
         <h1>Welcome to the Program Manager Page.</h1>
+        <div id="seasonTicketDisplay">
+            
+        </div>
     </body>
 </html>
---
+++ getSeasonTickets.php
@@ -0,0 +1,56 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "testdatabase";
+
+$link =  mysqli_connect($servername, $username, $password, $dbname);
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
+} 
+
+$result = mysqli_query($link,"SELECT * FROM seasonticket");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>Email</th>
+<th>Phone #</th>
+<th>Address</th>
+<th>Ticket #</th>
+<th>Seat #</th>
+<th>Day</th>
+<th>Time</th>
+<th>Edit</th>
+<th>Delete</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['firstname'] . "</td>";
+echo "<td>" . $row['lastname'] . "</td>";
+echo "<td>" . $row['email'] . "</td>";
+echo "<td>" . $row['phonenumber'] . "</td>";
+echo "<td>" . $row['address'] . "</td>";
+echo "<td>" . $row['ticketnumber'] . "</td>";
+echo "<td>" . $row['seat'] . "</td>";
+echo "<td>" . $row['day'] . "</td>";
+echo "<td>" . $row['time'] . "</td>";
+echo "<td><a href='javascript:void(0);' id='editLink'>Edit</a></td>";
+echo "<td><a href='javascript:void(0);' id='editLink'>Delete</a></td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($link);
+
+?>
