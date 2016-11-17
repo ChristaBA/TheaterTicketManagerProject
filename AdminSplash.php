a7071cb46eb1d97b3910282c292a6c788f143248
ChristaBA <bac0012@uah.edu>
2016-11-17 12:08:11 -0600

    Add files via upload

--- AdminSplash.php
+++ AdminSplash.php
@@ -8,11 +8,23 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Admin Splash Example</title>
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script>
+            $(document).ready(function() {
+                $.post("getAccounts.php", function(response){
+                   $("#accountDisplay").html(response);
+                   //console.log(response);
+                });
+            });
+        </script>
     </head>
     <body>
         <?php
         // put your code here
         ?>
         <h1>Welcome to the System Admin Page.</h1>
+        <div id="accountDisplay">
+            
+        </div>
     </body>
 </html>
